ccfe63a6906bf549c7154e7b2f515824ed41e281
Ibrokhimov <nexusinx00@gmail.com>
2024-09-23 18:00:36 +0500

    new site

---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+// src/components/Navbar.jsx
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1 className="navbar-title">
+        ГОСУДАРСТВЕННЫЙ КОМИТЕТ ПО АВТОМОБИЛЬНЫМ ДОРОГАМ
+        РЕСПУБЛИКИ УЗБЕКИСТАНА
+      </h1>
+      <div className="edit">EDIT</div>
+    </div>
+  );
+}
+
+export default Navbar;
